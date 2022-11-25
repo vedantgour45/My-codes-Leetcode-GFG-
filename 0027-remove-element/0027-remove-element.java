ab53be90c1e48ad75d7fcd4779e214628ae98119
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2022-11-25 11:26:34 +0530

    Time: 1 ms (50.70%), Space: 43 MB (8.84%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int i=0;
+        int j=0;
+        
+        while(j<n) {
+            if(nums[j]!=val) {
+                swap(nums, i, j);
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        
+        int count =0;
+        for(int k=0; k<n; k++) {
+            if(nums[k]!=val) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public void swap(int nums[], int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
