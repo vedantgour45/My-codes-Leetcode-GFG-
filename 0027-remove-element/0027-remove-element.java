3ebb3fe9e153379a1b47ce314e9b1cbbea5ef804
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2022-11-25 11:44:51 +0530

    Time: 1 ms (50.70%), Space: 42.6 MB (42.28%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,31 +1,40 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int n = nums.length;
-        int i=0;
-        int j=0;
+//         int i=0;
+//         int j=0;
         
-        while(j<n) {
-            if(nums[j]!=val) {
-                swap(nums, i, j);
-                i++;
-                j++;
-            } else {
-                j++;
-            }
-        }
+//         while(j<n) {
+//             if(nums[j]!=val) {
+//                 swap(nums, i, j);
+//                 i++;
+//                 j++;
+//             } else {
+//                 j++;
+//             }
+//         }
         
-        int count =0;
-        for(int k=0; k<n; k++) {
-            if(nums[k]!=val) {
-                count++;
-            }
-        }
-        return count;
-    }
+//         int count =0;
+//         for(int k=0; k<n; k++) {
+//             if(nums[k]!=val) {
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
     
-    public void swap(int nums[], int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+//     public void swap(int nums[], int i, int j) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+        
+        int idx = 0;    
+        for(int i = 0; i < n; i++){
+           if(nums[i] != val){
+               nums[idx++] = nums[i];
+           }
+        }
+   
+        return idx;
     }
 }
