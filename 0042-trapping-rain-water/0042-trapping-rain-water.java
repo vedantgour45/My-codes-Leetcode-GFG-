59175d1ea9e4469c436f2c6974c7f26f5b253fe1
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2022-11-27 15:37:09 +0530

    Time: 3 ms (37.20%), Space: 49.5 MB (13.02%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,24 +2,42 @@ class Solution {
     public int trap(int[] height) {
         int n = height.length;
         
-        int leftmax = height[0];
-        int rightmax = height[n-1];
+//         int leftmax = height[0];
+//         int rightmax = height[n-1];
+        
+//         int water = 0;
+        
+//         int left = 1;
+//         int right = n-2;
+        
+//         while(left <= right) {
+//             if(leftmax <= rightmax){
+//                 leftmax = Math.max(leftmax, height[left]);
+//                 water += leftmax-height[left];
+//                 left++;
+//             } else {
+//                 rightmax = Math.max(rightmax, height[right]);
+//                 water += rightmax - height[right];
+//                 right--;
+//             }
+//         }
+//         return water;
+        
+        int left[] = new int[n];
+        left[0] = height[0];
+        for(int i=1; i<n; i++) {
+            left[i] = Math.max(left[i-1], height[i]);
+        }
+        
+        int right[] = new int[n];
+        right[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--) {
+            right[i] = Math.max(right[i+1], height[i]);
+        }
         
         int water = 0;
-        
-        int left = 1;
-        int right = n-2;
-        
-        while(left <= right) {
-            if(leftmax <= rightmax){
-                leftmax = Math.max(leftmax, height[left]);
-                water += leftmax-height[left];
-                left++;
-            } else {
-                rightmax = Math.max(rightmax, height[right]);
-                water += rightmax - height[right];
-                right--;
-            }
+        for(int i=0; i<n; i++) {
+            water = water + (Math.min(left[i], right[i])-height[i]);
         }
         return water;
     }
