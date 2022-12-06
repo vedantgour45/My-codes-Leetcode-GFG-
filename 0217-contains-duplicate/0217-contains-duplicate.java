b8fa854dcc848983018b1cde1a465f1cae0fcc9d
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2022-12-06 19:38:29 +0530

    Time: 28 ms (36.83%), Space: 68.9 MB (49.63%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        boolean ans = false;
+        for(int i=0; i<nums.length-1; i++) {
+            if(nums[i]==nums[i+1]) {
+                ans = true;
+                break;
+            }
+        }
+        return ans;
+    }
+}
