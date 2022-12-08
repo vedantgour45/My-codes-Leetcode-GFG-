3340b778917c97af3ba6e5716661e5b59ae23346
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2022-12-08 09:26:29 +0530

    Time: 5 ms (13.20%), Space: 43.1 MB (12.65%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> st = new Stack<>();
+        for(String s : logs) {
+            if(s.equals("../")) {
+                if(!st.empty())
+                    st.pop();
+            } else if(s.equals("./")) {
+                continue;
+            } else {
+                st.push(s);
+            }
+        }
+        return st.size();
+    }
+}
