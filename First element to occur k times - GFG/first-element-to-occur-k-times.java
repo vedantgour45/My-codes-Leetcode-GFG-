d2243ccaddfdcc51b3d603c8ddd4357ae84bd3f4
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2022-12-14 14:25:00 +0530

    Added solution - LeetHub

---
+++ First element to occur k times - GFG/first-element-to-occur-k-times.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class GfG { 
+	public static void main (String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int m = Integer.parseInt(inputLine[1]);
+		    int[] arr = new int[n];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++)arr[i] = Integer.parseInt(inputLine[i]);
+		    
+		    Solution obj = new Solution();
+		    System.out.println(obj.firstElementKTime(arr, n, m));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public int firstElementKTime(int[] a, int n, int k) { 
+        // int freq[] = new int[1000001];
+        // for(int i=0; i<n; i++) {
+        //     freq[a[i]]++;
+        //     if(freq[a[i]]==k){
+        //         return a[i];
+        //     }
+        // }
+        // return -1;
+        
+        Map<Integer, Integer> hm = new HashMap<>();
+        for(int i=0; i<n; i++) {
+            if(hm.containsKey(a[i])) {
+                int oldFreq = hm.get(a[i]);
+                hm.put(a[i], oldFreq+1);
+            } else {
+                hm.put(a[i], 1);
+            }
+            
+            if(hm.get(a[i])==k) {
+                return a[i];
+            } 
+        }
+        return -1;
+    } 
+}
