9a25fbe70ca4d31108b79177d96bf44a47c7d66a
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2022-12-26 10:27:19 +0530

    Added solution - LeetHub

---
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution {
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int arr[], int n, int k) {
+        // 1. sum = 0, traverse the array
+        // 2. Add each number to the sum
+        // 3. If sum ==k, ans = i + 1
+        // 4. sum has crossed the value 'k', now we need to check if sum - k is
+        // there in the HashMap or not
+        // ans = Math.max (ans, length of subarray that has sum k)
+        
+        int sum = 0;
+        int ans = 0;
+        
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for(int i=0; i<n; i++) {
+            sum = sum + arr[i];
+            
+            if(sum == k) {
+                ans = i+1;
+            }
+            
+            if(!hm.containsKey(sum)) {
+                hm.put(sum, i);
+            }
+            
+            if(hm.containsKey(sum-k)) {
+                ans = Math.max(ans, i-hm.get(sum-k));
+            }
+        }
+        return ans;
+    }
+}
+
+
