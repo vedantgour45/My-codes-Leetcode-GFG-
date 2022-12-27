840d2c9520e45c0c4ca94d4274230b59482f23f8
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2022-12-27 10:57:58 +0530

    Time: 12 ms (99.57%), Space: 52 MB (96.56%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,33 +1,52 @@
 class Solution {
     public int longestConsecutive(int[] arr) {
-        
-        Map<Integer, Boolean> hm = new HashMap<>();
-        
-        for(int val : arr) {
-            hm.put(val, true);
-        }
-        
-         for(int val : arr) {
-           if(hm.containsKey(val-1)) {
-               hm.put(val, false);
-           }
-        }
-        
-        int maxLength = 0;
-        for(int val : arr) {
-            if(hm.get(val)==true) {
-                int tempLength = 1;
-                int tempLengthStartPoint = val;
+        Arrays.sort(arr);
+        int n = arr.length;
+        int count = 1;
+        int max = 1;
 
-                while(hm.containsKey(tempLengthStartPoint + tempLength)) {
-                    tempLength++;
-                }
+        if(n==1) 
+            return 1;
 
-                if(tempLength>maxLength) {
-                    maxLength = tempLength;
-                }
+        if(n==0) 
+            return 0;
+
+        for(int i=0;i<n-1;i++) {            
+            if(arr[i]==arr[i+1]-1) {
+                count++;
+                max=Math.max(max,count);
+            } else if(arr[i]==arr[i+1]) {
+                continue;
+            }
+            else {
+                count=1;
             }
         }
-        return maxLength;
+        return max;
     }
 }
+
+/*
+Arrays.sort(arr);
+int count = 1;
+int max = 1;
+
+if(n==1) 
+    return 1;
+
+if(n==0) 
+    return 0;
+
+for(int i=0;i<n-1;i++) {            
+    if(nums[i]==nums[i+1]-1) {
+        count++;
+        max=Math.max(max,count);
+    } else if(nums[i]==nums[i+1]) {
+        continue;
+    }
+    else {
+        count=1;
+    }
+}
+return max;
+*/
