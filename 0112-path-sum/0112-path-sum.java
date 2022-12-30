62d684216705b80f35742037ceae8992d704d812
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2022-12-30 09:38:49 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (71.97%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -19,8 +19,8 @@ class Solution {
             return false;
     
         if(root.left == null && root.right == null) 
-            return targetSum - root.val == 0;
-    
+            return targetSum == root.val;
+        
         return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 }
