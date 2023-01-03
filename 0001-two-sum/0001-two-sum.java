ad6efd4f7d1e71188b88e3b9e7857b2dcdbee1de
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2023-01-03 10:08:32 +0530

    Time: 76 ms (30.39%), Space: 45 MB (47.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,43 +4,39 @@ class Solution {
         int n = nums.length;
         int []ans = new int[2];
         
-        HashMap<Integer, Integer> hm = new HashMap<>();
+//         HashMap<Integer, Integer> hm = new HashMap<>();
         
-        for(int i=0; i<n; i++) {
+//         for(int i=0; i<n; i++) {
             
-            if(hm.containsKey(target-nums[i])) {
-                ans[0] = hm.get(target-nums[i]);
-                ans[1] = i;
-                return ans;
-            }
+//             if(hm.containsKey(target-nums[i])) {
+//                 ans[0] = hm.get(target-nums[i]);
+//                 ans[1] = i;
+//                 return ans;
+//             }
             
-            hm.put(nums[i], i);
+//             hm.put(nums[i], i);
+//         }
+        
+//         return ans;
+      
+        
+        int i = 0; 
+        int j = i+1;
+
+        while(i<j) {
+           if(nums[i]+nums[j]==target){
+               ans[0] = i;
+               ans[1] = j;
+               break;
+           } else if(j==n-1) {
+               i++;
+               j = i+1;
+           } else {
+               j++;
+           }
         }
-        
+
         return ans;
-        
-        
-        
-        
-        
-        
-        
-        // int i = 0; 
-        // int j = n-1;
-
-        // while(i<j) {
-        //    if(nums[i]+nums[j]>target){
-        //        j--;
-        //    } else if(nums[i]+nums[j]<target) {
-        //        i++;
-        //    } else {
-        //        ans[0] = i;
-        //        ans[1] = j;
-        //        break;
-        //    }
-        // }
-
-        // return ans;
             
         // for(int i=0; i<n; i++) {
         //     for(int j=i+1; j<n; j++) {
