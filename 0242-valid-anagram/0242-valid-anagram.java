6991dd4045728e5d4c7f25baffb07dfe6f868482
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2023-01-07 17:59:44 +0530

    Time: 12 ms (49.07%), Space: 42.9 MB (66.10%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -36,14 +36,30 @@ class Solution {
                 return true;
                 */
         
-        int freq[] = new int[26];      // most optimised approach
+//         int freq[] = new int[26];      // most optimised approach
         
-        for(int i=0; i<s.length(); i++){
-            freq[s.charAt(i)-'a']++;
-            freq[t.charAt(i)-'a']--;
+//         for(int i=0; i<s.length(); i++){
+//             freq[s.charAt(i)-'a']++;
+//             freq[t.charAt(i)-'a']--;
+//         }
+//         for(int i=0; i<26; i++){
+//             if(freq[i]!=0){
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for(int i=0; i<s.length(); i++) {
+            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0)+1);
         }
-        for(int i=0; i<26; i++){
-            if(freq[i]!=0){
+        
+         for(int i=0; i<t.length(); i++) {
+             hm.put(t.charAt(i), hm.getOrDefault(t.charAt(i), 0)-1);           
+        }
+        
+        for(char c : hm.keySet()) {
+            if(hm.get(c)<0 || hm.get(c)>0) {
                 return false;
             }
         }
