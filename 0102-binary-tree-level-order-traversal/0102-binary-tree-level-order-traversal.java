e0634fe0afbd2ba8faf138b6526e44efda37c8ce
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2023-01-09 18:51:25 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (72.55%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,36 +15,38 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        // Template for all BFS -> Graphs
-        // BFS -> Breadth First Search
-        // DFS -> Depth First Search
         
         List<List<Integer>> ans = new ArrayList<>();
-        if(root == null) 
+        
+        if(root == null) {
             return ans;
+        }
         
-        // 1. Start with adding the root
         Queue<TreeNode> q = new LinkedList<>();
-        
         q.add(root);
         
-        while(q.size() > 0) {
-            // In every level perform RPA for every mode
-            // remove, print, add child
+        while(!q.isEmpty()) {
             
             int size = q.size();
             
-            List<Integer> level = new ArrayList<>();
-            for(int i = 0; i < size; i++) {
-                TreeNode temp = q.remove();
-                level.add(temp.val);
-                if(temp.left != null) 
-                    q.add(temp.left);
-                if(temp.right != null) 
-                    q.add(temp.right);
+            List<Integer> currLevel = new ArrayList<>();
+            
+            for(int i=0; i<size; i++) {
+                //Perform RPA
+                
+                //Remove
+                TreeNode temp = q.poll();
+                
+                //Print(add add to ans)
+                currLevel.add(temp.val);
+                
+                //Add clild nodes to Queue
+                if(temp.left != null) q.add(temp.left);
+                if(temp.right != null) q.add(temp.right);
             }
-            ans.add(level);
+            ans.add(currLevel);
         }
+        
         return ans;
     }
 }
