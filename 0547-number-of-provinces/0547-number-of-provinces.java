b73c485979dc11e12d171985b77d7a0c344085a1
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2023-01-20 10:41:30 +0530

    Time: 3 ms (45.77%), Space: 43.1 MB (71.62%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    public static void DFS(int src, ArrayList<Integer>[] adjList, boolean visited[]) {
+        visited[src] = true;
+        
+        for(int n : adjList[src]) {
+            if(visited[n] == false){
+                DFS(n, adjList, visited);
+            }
+        }
+    }
+    
+    public int findCircleNum(int[][] isConnected) {
+        int vertices = isConnected.length; 
+        ArrayList<Integer>[] adjList = new ArrayList[vertices];
+        
+        for(int i=0; i<vertices; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+        
+        // given input is a list of list i.e. 2D array
+        // we need to change it into adjecency list;
+        
+        for(int i=0; i<vertices; i++) {
+            for(int j=0; j<vertices; j++) {
+                if(isConnected[i][j]==1 && i!=j) {
+                    adjList[i].add(j);
+                    adjList[j].add(i);
+                }
+            }
+        }
+        
+        boolean visited[] = new boolean[vertices];
+        int count = 0;
+        
+        for(int i=0; i<vertices; i++) {
+            if(visited[i] == false) {
+                count++;
+                DFS(i, adjList, visited);
+            }
+        }
+        return count;
+    }
+}
