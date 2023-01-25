d0605bc9ebbd713c102b819cf298d2fc12991455
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2023-01-25 10:54:15 +0530

    Time: 137 ms (56.64%), Space: 134.3 MB (81.52%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,46 +1,40 @@
 class Solution {
-    
-    public boolean dfs(int src, int dest, ArrayList<Integer>[] adjList, boolean[] visited) {
-        
-        if(src == dest) {
-            return true;
-        }
-        
+
+    private boolean dfs(int src, int dest, ArrayList<Integer>[] graph, boolean[] visited) {
+
+        if(src == dest) return true;
+
         visited[src] = true;
-        
-        for(int nbr : adjList[src]) {
-            if(visited[nbr] == false) {
-                //now the neighbour will check for the destination
-                boolean exist = dfs(nbr, dest, adjList, visited); 
-                if(exist) {
-                    return true;
-                } 
+        for(int nbr : graph[src]) {
+            if(visited[nbr]==false) {
+                boolean pathExists = dfs(nbr, dest, graph, visited);
+
+                if(pathExists) return true;
             }
         }
         return false;
     }
-    
+
     public boolean validPath(int vertices, int[][] edges, int src, int dest) {
-        
-        // 1. create adjecency list(graph)
+        // 1. build an adjecency list / graph
         ArrayList<Integer>[] graph = new ArrayList[vertices];
-        
+
         for(int i=0; i<vertices; i++) {
             graph[i] = new ArrayList<>();
         }
-        
-        for(int[] arr : edges) {
-            int u = arr[0];
-            int v = arr[1];
-            
+
+        for(int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
             graph[u].add(v);
             graph[v].add(u);
         }
-        
-        // 2. make a boolean array visited
+
+        // 2. make boolean array visited
         boolean[] visited = new boolean[vertices];
-        
-        // 3. call dfs
-        return dfs(src, dest, graph, visited);     
+
+        // 3. call dfs and return
+        return dfs(src, dest, graph, visited);
     }
 }
