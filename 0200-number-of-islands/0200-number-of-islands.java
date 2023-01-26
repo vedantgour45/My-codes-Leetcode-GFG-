fe9c0fa6d5217ce01cd579d6b58c5e49a6748da0
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2023-01-26 22:16:37 +0530

    Time: 4 ms (63.52%), Space: 51.2 MB (50.70%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,43 @@
 class Solution {
+
+    private int[] dr = {-1, 0, 1, 0};
+    private int[] dc = {0, 1, 0, -1};
+
+    private void dfs(int r, int c, char[][] grid, boolean[][] visited) {
+
+        int row = grid.length;
+        int col = grid[0].length;
+
+        visited[r][c] = true;
+
+        for(int i=0; i<4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if(nr<0 || nr>=row || nc<0 || nc>=col) continue;
+
+            if(visited[nr][nc]==false && grid[nr][nc]=='1') {
+                dfs(nr, nc, grid, visited);
+            }
+        }
+    }
+
     public int numIslands(char[][] grid) {
+
+        int row = grid.length;
+        int col = grid[0].length;
         int count = 0;
-        
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == '1') {
+
+        boolean[][] visited = new boolean[row][col];
+
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(visited[i][j]==false && grid[i][j]=='1') {
+                    dfs(i, j, grid, visited);
                     count++;
-                    clearRestOfLand(grid, i, j);
                 }
             }
         }
         return count;
-    }
-    
-    private void clearRestOfLand(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0') return;
-        
-        grid[i][j] = '0';
-        clearRestOfLand(grid, i+1, j);
-        clearRestOfLand(grid, i-1, j);
-        clearRestOfLand(grid, i, j+1);
-        clearRestOfLand(grid, i, j-1);
-        return;
     }
 }
