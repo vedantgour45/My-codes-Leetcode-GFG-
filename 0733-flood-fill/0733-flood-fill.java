302f35a3ed759389404b708fab960fd6cd579b0b
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2023-01-26 18:33:39 +0530

    Time: 1 ms (62.38%), Space: 43.4 MB (56.98%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,16 +1,37 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        if (image[sr][sc] == newColor) return image;
-        fill(image, sr, sc, image[sr][sc], newColor);
-        return image;
+    
+    private int[] dr = {-1, 0, 1, 0};
+    private int[] dc = {0, 1, 0, -1};
+    
+    private void dfs(int[][] image, int r, int c, int newColor, boolean[][] visited) {
+        
+        visited[r][c] = true;
+        int originalColor = image[r][c];
+        image[r][c] = newColor;
+        
+        int row = image.length;
+        int col = image[0].length;
+        
+        for(int i=0; i<4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            
+            if(nr<0 || nr>=row || nc<0 || nc>=col) continue;
+            
+            if(visited[nr][nc] == false && image[nr][nc]==originalColor) {
+                dfs(image, nr, nc, newColor, visited);
+            }
+        }
     }
     
-    private void fill(int[][] image, int sr, int sc, int color, int newColor) {
-        if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) return;
-        image[sr][sc] = newColor;
-        fill(image, sr + 1, sc, color, newColor);
-        fill(image, sr - 1, sc, color, newColor);
-        fill(image, sr, sc + 1, color, newColor);
-        fill(image, sr, sc - 1, color, newColor);
+    public int[][] floodFill(int[][] image, int r, int c, int newColor) {
+        
+        int row = image.length;
+        int col = image[0].length;
+        
+        boolean[][] visited = new boolean[row][col];
+        
+        dfs(image, r, c, newColor, visited);
+        return image;
     }
 }
