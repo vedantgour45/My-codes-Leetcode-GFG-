a241bb0b454e1aa85f094949ed9510faf99c351f
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2023-01-26 16:40:44 +0530

    Time: 16 ms (87.31%), Space: 50 MB (83.16%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -0,0 +1,51 @@
+class Solution {
+    
+    private boolean bfs(int src, ArrayList<Integer>[] graph, boolean[] visited, int[] color) {
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(src);
+        visited[src] = true;
+        color[src] = 0;
+        
+        while(!q.isEmpty()) {
+            int temp = q.remove();
+            
+            for(int nbr : graph[temp]) {
+                if(visited[nbr] == false) {
+                    q.add(nbr);
+                    visited[nbr] = true;
+                    color[nbr] = 1-color[temp];
+                }
+                else if(visited[nbr] == true && color[nbr] == color[temp]) return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean possibleBipartition(int vertices, int[][] dislikes) {
+        
+        ArrayList<Integer>[] graph = new ArrayList[vertices];
+        
+        for(int i=0; i<vertices; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for(int[] edge: dislikes) {
+            int u = edge[0];
+            int v = edge[1];
+            
+            graph[u-1].add(v-1);
+            graph[v-1].add(u-1);
+        }
+        
+        boolean[] visited = new boolean[vertices];
+        int[] color = new int[vertices];
+        
+        for(int i=0; i<vertices; i++) {
+            if(visited[i] == false) {
+                if(!bfs(i, graph, visited, color)) return false;
+            }
+        }
+        return true;
+    }
+}
