30eb9d9c53fc9ef55002bc803cfe09b7423f57ab
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2023-01-28 18:10:56 +0530

    Time: 1 ms (7.92%), Space: 45.9 MB (7.44%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.java
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.java
@@ -17,14 +17,12 @@
 class Solution {
     public TreeNode trimBST(TreeNode root, int low, int high) {
         
-        if( root == null)
-        return root ; 
+        if( root == null) return root ; 
 
         root.left = trimBST(root.left, low, high);
         root.right = trimBST(root.right, low, high);
         
-        if(root.val < low || root.val> high)
-        {
+        if(root.val < low || root.val> high) {
             if(root.left == null)
             return root.right;
             
