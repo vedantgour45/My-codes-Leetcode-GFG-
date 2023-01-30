725ae2c1f6922dfb474100c5c3f6b55cc9a0d2db
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2023-01-30 11:52:59 +0530

    Time: 3 ms (91.82%), Space: 43 MB (62.61%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -15,17 +15,17 @@
  */
 class Solution {
     
-    HashSet<Integer> map = new HashSet<>();
+    HashSet<Integer> set = new HashSet<>();
     
     public boolean findTarget(TreeNode root, int k) {
         if(root == null) {
             return false;
         }
         
-        if(map.contains(k-root.val)) {
+        if(set.contains(k-root.val)) {
             return true;
         }
-        map.add(root.val);
+        set.add(root.val);
         
         return findTarget(root.left, k) || findTarget(root.right, k);
     }
