631178db3f4e49e5f02a19313ac393b150036fcd
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2023-01-30 11:26:40 +0530

    Time: 2 ms (99.87%), Space: 43.9 MB (57.21%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,21 +1,16 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        //Three variables needed for our sliding window
 
         int start = 0;
         int count = 0;
-        int res = 0;
-        //iterate through the length of our array to find an update
-        //our longest subarray
+        int ans = 0;
 
-        for(int end = 0;end<nums.length;end++){
-
-            //if we find a 0, we update our current
+        for(int end = 0; end<nums.length; end++){
+            
             if(nums[end]==0){
                 count++;
             }
 
-            //if we find more than 2 0's, we need to adjust window
             while(count > k){
                 if(nums[start]==0){
                    count--;
@@ -23,11 +18,9 @@ class Solution {
                start++;
             }
 
-            //update the lenght of our subarray every iteration to
-            //to find the maximum length
-            res = Math.max(res, end-start+1);
+            ans = Math.max(ans, end-start+1);
          }
-        return res;
+        return ans;
 
     }
 }
