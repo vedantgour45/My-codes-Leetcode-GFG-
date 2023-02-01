1e4029e1649ec833762843534121e5a732324684
Vedant Gour <113295244+vedant4545@users.noreply.github.com>
2023-02-01 17:10:18 +0530

    Time: 1 ms (85.83%), Space: 41.8 MB (84.84%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        
+        if(!(str1+str2).equals(str2+str1)) return ""; // because they will not have GCD
+        
+        int gcd = getGCD(str1.length(), str2.length());
+        
+        return str1.substring(0, gcd);
+    }
+
+    public int getGCD(int a, int b) {
+        
+        // if (b==0) return a;
+        // return getGCD(b,a%b);
+        
+        while(a % b != 0){
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+        
+        int GCD = b;
+        return GCD;
+    }
+}
