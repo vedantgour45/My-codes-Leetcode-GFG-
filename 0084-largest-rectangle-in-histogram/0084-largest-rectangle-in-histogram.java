89934cfdcbc0f25b6ed4ebbf54f179c07ef9d003
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-03-02 22:09:52 +0530

    Time: 275 ms (27.82%), Space: 59.5 MB (29.96%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,5 +1,59 @@
 class Solution {
+    
+    static int[] nextSmaller(int arr[], int n) {
+        int ans[] = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = n - 1; i >= 0; i--){
+            while(!st.empty() && arr[i] <= arr[st.peek()]){
+                st.pop();
+            }
+            if(!st.empty()){
+                ans[i] = st.peek(); // index of next Smaller element
+            }
+            else { // arr[i] doesn't have Next smaller element
+                ans[i] = n; // MOST IMPORTANT
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    
+    static int[] prevSmaller(int arr[], int n) {
+        
+        int ans[] = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && arr[i] <= arr[st.peek()]){
+                st.pop();
+            }
+            if(!st.empty()){
+                ans[i] = st.peek(); // index of next Smaller element
+            }
+            else { // arr[i] doesn't have previous smaller element
+                ans[i] = -1; // MOST IMPORTANT
+            }
+            st.push(i);
+        }
+        return ans;
+      }
+    
     public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int nexts[] = nextSmaller(heights, n);
+        int prevs[] = prevSmaller(heights, n);
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            ans = Math.max(ans, (nexts[i] - prevs[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
+}
+
+
+
+/*
+
         int area = 0;
         int n=heights.length;
         
@@ -27,5 +81,5 @@ class Solution {
         }
 
         return area;
-    }
-}
+
+*/
