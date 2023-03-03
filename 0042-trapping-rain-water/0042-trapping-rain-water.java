7f45e96e4074bd1810a15745bb9ac10c59ed669a
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-03-03 11:40:40 +0530

    Time: 1 ms (98.35%), Space: 43.5 MB (25.41%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,27 +1,61 @@
 class Solution {
     public int trap(int[] height) {
-//         int n = height.length;
+
+        int n = height.length;
         
-// //         int leftmax = height[0];
-// //         int rightmax = height[n-1];
+        int[] leftMax = new int[n]; // stores the maximum height on the left of each bar
+        int[] rightMax = new int[n]; // stores the maximum height on the right of each bar
         
-// //         int water = 0;
+        // find the maximum height on the left of each bar
+        int max = 0;
+        for (int i=0; i<n; i++) {
+            leftMax[i] = max;
+            max = Math.max(max, height[i]);
+        }
         
-// //         int left = 1;
-// //         int right = n-2;
+        // find the maximum height on the right of each bar
+        max = 0;
+        for (int i=n-1; i>=0; i--) {
+            rightMax[i] = max;
+            max = Math.max(max, height[i]);
+        }
         
-// //         while(left <= right) {
-// //             if(leftmax <= rightmax){
-// //                 leftmax = Math.max(leftmax, height[left]);
-// //                 water += leftmax-height[left];
-// //                 left++;
-// //             } else {
-// //                 rightmax = Math.max(rightmax, height[right]);
-// //                 water += rightmax - height[right];
-// //                 right--;
-// //             }
-// //         }
-// //         return water;
+        // calculate the amount of water trapped at each bar
+        int water = 0;
+        for (int i=0; i<n; i++) {
+            int minHeight = Math.min(leftMax[i], rightMax[i]);
+            if (height[i] < minHeight) {
+                water += minHeight - height[i];
+            }
+        }
+        return water;
+    }
+}
+
+
+//          int n = height.length;   
+//         int leftmax = height[0];
+//         int rightmax = height[n-1];
+        
+//         int water = 0;
+        
+//         int left = 1;
+//         int right = n-2;
+        
+//         while(left <= right) {
+//             if(leftmax <= rightmax){
+//                 leftmax = Math.max(leftmax, height[left]);
+//                 water += leftmax-height[left];
+//                 left++;
+//             } else {
+//                 rightmax = Math.max(rightmax, height[right]);
+//                 water += rightmax - height[right];
+//                 right--;
+//             }
+//         }
+//         return water;
+
+
         
 //         int left[] = new int[n];
 //         left[0] = height[0];
@@ -40,33 +74,3 @@ class Solution {
 //             water = water + (Math.min(left[i], right[i])-height[i]);
 //         }
 //         return water;
-        int n = height.length;
-        int[] leftMax = new int[n]; // stores the maximum height on the left of each bar
-        int[] rightMax = new int[n]; // stores the maximum height on the right of each bar
-        
-        // find the maximum height on the left of each bar
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            leftMax[i] = max;
-            max = Math.max(max, height[i]);
-        }
-        
-        // find the maximum height on the right of each bar
-        max = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            rightMax[i] = max;
-            max = Math.max(max, height[i]);
-        }
-        
-        // calculate the amount of water trapped at each bar
-        int water = 0;
-        for (int i = 0; i < n; i++) {
-            int minHeight = Math.min(leftMax[i], rightMax[i]);
-            if (height[i] < minHeight) {
-                water += minHeight - height[i];
-            }
-        }
-        
-        return water;
-    }
-}
