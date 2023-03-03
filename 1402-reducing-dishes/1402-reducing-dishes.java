75745bfa7d501694b84aef331985e7bfab39c018
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-03-03 18:59:40 +0530

    Time: 1 ms (100.00%), Space: 40.9 MB (48.84%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -4,7 +4,7 @@ class Solution {
         Arrays.sort(satisfaction);
         
         int res = 0;
-        int total = 0;
+        int total = 0; //total keeps track of the total satisfaction obtained so far
         int n = satisfaction.length;
         
         for (int i=n-1; i>=0; i--) {
@@ -16,3 +16,9 @@ class Solution {
         return res;
     }
 }
+
+/*
+
+since we are iterating through the satisfaction array in reverse order, we need to check if satisfaction[i] - total is greater than zero, which is why we check if satisfaction[i] > -total.
+
+*/
