eb07512fa95b547092a9120fac5673086cd8392b
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-03-03 16:13:44 +0530

    Time: 1 ms (100.00%), Space: 40.2 MB (85.84%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSatisfaction(int[] satisfaction) {
+        
+        Arrays.sort(satisfaction);
+        
+        int res = 0;
+        int total = 0;
+        int n = satisfaction.length;
+        
+        for (int i=n-1; i>=0; --i) {
+            if(satisfaction[i] > -total) {
+                total += satisfaction[i];
+                res += total;
+            }
+        }
+        return res;
+    }
+}
