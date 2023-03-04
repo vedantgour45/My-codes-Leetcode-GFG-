8946c8c8da141ea6d331d22acd3914aadc85e59d
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-03-04 10:42:40 +0530

    Time: 12 ms (83.86%), Space: 43.2 MB (47.31%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,18 +1,21 @@
 class Solution {
     public int partitionString(String s) {
         
-        int[] freq = new int[26];
+        //pos[] to keep track of the most recent position of each letter in the input string.
+        int[] pos = new int[26];
         
         int res = 0;
+        //last represents the last index of the current substring being processed.
         int last = 0;
         
         for (int i=0; i<s.length(); i++) {
-            if (freq[s.charAt(i)-'a'] >= last) {
-                res++;
-                last = i+1;
+            //If the value is greater than or equal to the current value of "last", then the current character must be included in the next substring. 
+            if (pos[s.charAt(i)-'a'] >= last) {
+                res++; //to indicate that a new substring has been found,
+                last = i+1;  //sets the value of "last" to the index of the next character after the current character.
             }
             
-            freq[s.charAt(i)-'a'] = i+1;
+            pos[s.charAt(i)-'a'] = i+1;
         }
         return res;
     }
