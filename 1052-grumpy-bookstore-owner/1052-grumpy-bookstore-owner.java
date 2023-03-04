c53e6187484297fd584671d5abdaec4c7cc1f961
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-03-04 11:08:44 +0530

    Time: 4 ms (32.87%), Space: 45.5 MB (33.05%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -9,15 +9,17 @@ class Solution {
             
             customers[i] = grumpy[i]*customers[i]; // collect the unsatisfy customers
         }
-        
-        
+    
         return satisfy + getMax(customers, minutes);
     }
     
     public int getMax(int[] arr, int k){ // k is window size
+        
         int max = 0;
         int sum = 0;
-        for(int i=0;i<arr.length;i++){
+        
+        for(int i=0; i<arr.length;  i++){
+            
             sum += arr[i];
             if(i>=k){
                 sum -= arr[i-k]; 
