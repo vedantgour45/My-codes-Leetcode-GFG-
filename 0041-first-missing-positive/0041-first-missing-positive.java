73e3f9d7a055536ddeab313e2ff2ffc2433f3481
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-03-05 13:08:58 +0530

    Time: 11 ms (16.18%), Space: 50.7 MB (54.30%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -17,7 +17,7 @@ class Solution {
         
         for(int i=0; i<nums.length; i++){
             if(!map.containsKey(nums[i])){
-                map.put(nums[i], 0);
+                map.put(nums[i], 1);
             }
         }
 
