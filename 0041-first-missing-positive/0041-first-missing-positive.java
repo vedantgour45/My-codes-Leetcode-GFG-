2d6d43d4834cd1ce8f16a89786c54bb05fb5baae
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-03-06 11:14:18 +0530

    Time: 1 ms (99.98%), Space: 51 MB (35.37%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,13 +1,45 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
-        int missing = 1;
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i]==missing) {
-                missing++;
+    public int firstMissingPositive(int[] arr) {
+
+        int n = arr.length;
+        
+        // mark element which are out of range and manage presence of one
+        boolean one = false;
+        for (int i=0; i<n; i++) {
+            if(arr[i]==1) {
+                one = true;
+            }
+            //out of range
+            if(arr[i]<1 || arr[i]>n){
+                arr[i]=1;
             }
         }
-        return missing;
+        if(one==false) return 1;
+        
+        // Map element with index
+        for(int i=0; i<n; i++) {
+            int idx = Math.abs(arr[i]);
+            arr[idx-1] = -Math.abs(arr[idx-1]);
+        }
+        
+        // Find the first missing positive integer
+        for (int i=0; i<n; i++) {
+            // if element is +ve that means index +1 is missing from array
+            if(arr[i]>0){
+                return i+1;
+            }
+        }
+        return n+1; 
+        
+        
+        // Arrays.sort(nums);
+        // int missing = 1;
+        // for(int i=0; i<nums.length; i++) {
+        //     if(nums[i]==missing) {
+        //         missing++;
+        //     }
+        // }
+        // return missing;
         
         
         
