272871d564ef86e5c2803efb8d780976fe5418f8
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-05-26 09:21:20 +0530

    Time: 62 ms (82.20%), Space: 44.9 MB (26.83%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+  let currentCount = init;
+  return {
+    increment: function() {
+       currentCount++;
+       return currentCount;
+    },
+    decrement: function() {
+       currentCount--;
+       return currentCount;
+    },
+    reset: function() {
+       currentCount = init;
+       return currentCount;
+    },
+  }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
