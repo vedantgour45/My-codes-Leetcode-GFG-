7bc1134f360493772ca61e34b93fc437593c774d
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-08-30 15:14:04 +0530

    Time: 74 ms (77.87%), Space: 46.4 MB (79.21%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -21,3 +21,29 @@ var lengthOfLongestSubstring = function(s) {
     }
     return maxSize;
 };
+
+/* 
+var longestSubstring = function(s) {
+    let set = new Set();
+    let left = 0;
+    let maxSize = 0;
+    let longestSubstr = '';
+
+    if (s.length === 0) return '';
+    if (s.length === 1) return s;
+
+    for (let i = 0; i < s.length; i++) {
+        while (set.has(s[i])) {
+            set.delete(s[left]);
+            left++;
+        }
+        set.add(s[i]);
+        
+        if (i - left + 1 > maxSize) {
+            maxSize = i - left + 1;
+            longestSubstr = s.substring(left, i + 1);
+        }
+    }
+    return longestSubstr;
+};
+*/
