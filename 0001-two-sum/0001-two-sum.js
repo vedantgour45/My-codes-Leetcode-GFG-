8eaec551c06847a54ccbabc4289bc60f74b2cd71
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-11-21 12:15:20 +0530

    Time: 48 ms (95.22%), Space: 43 MB (31.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,25 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let ans = [];
-    for(let i=0; i<nums.length-1; i++) {
-        for(let j=i+1; j<nums.length; j++) {
-            if(nums[i]+nums[j]===target) {
-                ans[0] = i;
-                ans[1] = j;
-            }
-        }
-    }
+//     let ans = [];
+//     for(let i=0; i<nums.length-1; i++) {
+//         for(let j=i+1; j<nums.length; j++) {
+//             if(nums[i]+nums[j]===target) {
+//                 ans[0] = i;
+//                 ans[1] = j;
+//             }
+//         }
+//     }
     
-    return ans;
+//     return ans;
+    
+    const map = new Map();
+    for(let i=0; i<nums.length; i++) {
+        const element = target-nums[i];
+        if(map.has(element)) {
+            return [map.get(element), i]
+        }
+        map.set(nums[i], i)
+    }
+    return [];
 };
